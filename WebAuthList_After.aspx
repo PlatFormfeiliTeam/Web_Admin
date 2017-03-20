6744087540cbc38c97c919aa5bd6a445ae366306
heguiqin <guiqin_he@163.com>
2017-03-20 11:29:16 +0800

    后台权限管理

---
+++ WebAuthList_After.aspx
@@ -0,0 +1,207 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="WebAuthList_After.aspx.cs" Inherits="Web_Admin.WebAuthList_After" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+        <link href="/Extjs42/resources/css/ext-all-gray.css" rel="stylesheet" type="text/css" />
+    <script src="/Extjs42/bootstrap.js" type="text/javascript"></script>
+    <script src="/js/pan.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        var gridUser, store_user, treeModel, treeModelstore, gridStation, store_Station;
+        var userid = '';
+        Ext.onReady(function () {
+            Ext.regModel('User', { fields: ['ID', 'NAME', 'REALNAME'] })
+            store_user = Ext.create('Ext.data.JsonStore', {
+                model: 'User',
+                proxy: {
+                    type: 'ajax',
+                    url: 'WebAuthList_After.aspx?action=loaduser',
+                    reader: {
+                        root: 'rows',
+                        type: 'json'
+                    }
+                },
+                autoLoad: true
+            })
+
+            gridUser = Ext.create('Ext.grid.Panel', {
+                width: 500,
+                height: 500,
+                renderTo: "div_west",
+                store: store_user,
+                columns: [
+                    { xtype: 'rownumberer', width: 35 },
+                    { header: 'ID', dataIndex: 'ID', hidden: true },
+                    { header: '登录名', dataIndex: 'NAME', width: 180 },
+                    { header: '姓名', dataIndex: 'REALNAME', width: 180 }
+                ],
+                listeners: {
+                    itemclick: function (value, record, item, index, e, eOpts) {
+                        treeModelstore.setProxy({
+                            type: 'ajax',
+                            url: 'WebAuthList.aspx?action=loadauthority',
+                            reader: 'json'
+                        });
+                        userid = record.get("ID");
+                        var proxys = treeModelstore.proxy;
+                        proxys.extraParams.userid = userid;
+                        treeModelstore.load();
+                    }
+                }
+            })
+
+            var myMask = new Ext.LoadMask(Ext.getBody(), { msg: "数据加载中，请稍等..." });
+
+            //系统模块
+            Ext.regModel("SysModelAuth", { fields: ["id", "name", "leaf", "url", "ParentID"] });
+            treeModelstore = new Ext.data.TreeStore({
+                model: 'SysModelAuth',
+                nodeParam: 'id',
+                proxy: {
+                    type: 'ajax',
+                    url: 'WebAuthList_After.aspx?action=loadauthority',
+                    reader: 'json'
+                },
+                root: {
+                    expanded: true,
+                    name: '后端模块',
+                    id: '91a0657f-1939-4528-80aa-91b202a593ac'
+                },
+                listeners: {
+                    beforeload: function () {
+                        myMask.show();
+                    },
+                    load: function (st, rds, opts) {
+                        if (myMask) { myMask.hide(); }
+                    }
+                }
+            });
+            treeModel = Ext.create('Ext.tree.Panel', {
+                useArrows: true,
+                animate: true,
+                rootVisible: false,
+                renderTo: "div_east",
+                store: treeModelstore,
+                height: 500,
+                // width: 600,
+                columns: [
+                { text: 'id', dataIndex: 'id', width: 500, hidden: true },
+                { text: 'leaf', dataIndex: 'leaf', width: 100, hidden: true },
+                { header: '模块名称', xtype: 'treecolumn', text: 'name', dataIndex: 'name', flex: 1 },
+                { text: 'ParentID', dataIndex: 'ParentID', width: 100, hidden: true }
+                ],
+                listeners: {
+                    'checkchange': function (node, checked) {
+                        setChildChecked(node, checked);
+                        setParentChecked(node, checked);
+                    }
+                }
+            });
+            //var tbar = Ext.create('Ext.toolbar.Toolbar', {
+            //    renderTo: 'toolbar',
+            //    items: ['->', '<button onclick="SaveAuthorization()" type="button"><span class="icon iconfont" onclick="SaveAuthorization()">&#xe60c;</span>&nbsp;保 存</button>']
+            //})
+
+            //======================联动选择==========================
+            /*向上遍历父结点*/
+            var nodep = function (node) {
+                var bnode = true;
+                Ext.Array.each(node.childNodes, function (v) {
+                    if (!v.data.checked) {
+                        bnode = true;
+                        return;
+                    }
+                });
+                return bnode;
+            };
+            var parentnode = function (node) {
+                if (node.parentNode != null) {
+                    if (nodep(node.parentNode)) {
+                        node.parentNode.set('checked', true);
+                    } else {
+                        node.parentNode.set('checked', false);
+                    }
+                    parentnode(node.parentNode);
+                }
+            };
+            /*遍历子结点 选中 与取消选中操作*/
+            var chd = function (node, check) {
+                node.set('checked', check);
+                if (node.isNode) {
+                    node.eachChild(function (child) {
+                        chd(child, check);
+                    });
+                }
+            };
+        });
+
+        //选择子节点
+        function setChildChecked(node, checked) {
+            node.expand();
+            node.set('checked', checked);
+            if (node.hasChildNodes()) {
+                node.eachChild(function (child) {
+                    setChildChecked(child, checked);
+                });
+            }
+        }
+
+        //选择父节点
+        function setParentChecked(node, checked) {
+            node.set({ checked: checked });
+            var parentNode = node.parentNode;
+            if (parentNode != null) {
+                var flag = false;
+                parentNode.eachChild(function (childnode) {
+                    if (childnode.get('checked')) {
+                        flag = true;
+                    }
+                });
+                if (checked == false) {
+                    if (!flag) {
+                        setParentChecked(parentNode, checked);
+                    }
+                } else {
+                    if (flag) {
+                        setParentChecked(parentNode, checked);
+                    }
+                }
+            }
+        }
+
+        function SaveAuthorization() {
+            if (userid) {
+                var moduleids = "";
+                var recs = treeModel.getChecked();
+                for (var i = 0; i < recs.length; i++) {
+                    moduleids += recs[i].data.id + ',';
+                }
+                var mask = new Ext.LoadMask(Ext.getBody(), { msg: "保存当前账户数据并同步更新子账号数据中，请稍等..." });
+                mask.show();
+                Ext.Ajax.request({
+                    timeout: 1000000000,
+                    url: 'WebAuthList_After.aspx?action=AuthorizationSave',
+                    params: { moduleids: moduleids, userid: userid },
+                    success: function (option, success, response) {
+                        if (option.responseText == '{success:true}') {
+                            Ext.MessageBox.alert('提示', '保存成功！');
+                        } else {
+                            Ext.MessageBox.alert('提示', '保存失败！');
+                        }
+                        mask.hide();
+                    }
+                })
+            }
+            else {
+                Ext.MessageBox.alert('提示', '请先选择需要授权的账号！');
+            }
+        }
+
+    </script>
+    <div class="btn-group" role="group">
+        <button type="button" onclick="SaveAuthorization()" class="btn btn-primary btn-sm"><i class="icon iconfont">&#xe60c;</i>&nbsp;保存</button>
+    </div>
+    <div>
+        <div id="div_west" style="float: left; width: 35%">
+        </div>
+        <div id="div_east" style="float: left; width: 65%">
+        </div>
+    </div>
+</asp:Content>
---
+++ WebAuthList_After.aspx.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Web_Admin.Common;
+using Newtonsoft.Json;
+using System.Data;
+
+namespace Web_Admin
+{
+    public partial class WebAuthList_After : System.Web.UI.Page
+    {
+        string action = string.Empty;
+        string userid = string.Empty;
+        string ParentID = string.Empty;
+        string GroupId = string.Empty;
+        string AllId = string.Empty;
+        string type = "";
+        string sql = string.Empty; DataTable ents;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            action = Request["action"];
+            ID = Request["id"]; userid = Request["userid"]; ParentID = Request["parentid"];
+            GroupId = Request["groupid"]; AllId = Request["allid"]; type = Request["type"];
+
+            string result = "";
+
+            switch (action)
+            {
+                case "loadauthority":
+                    string sql = string.Empty;
+                    if (!string.IsNullOrEmpty(userid))
+                    {
+                        sql = @"select t.*,u.MODULEID AUTHORITY from sysmodule t  left join (select * from sys_moduleuser where userid='{0}') u on t.MODULEID=u.MODULEID
+                              where  t.ParentId='{1}' order by t.SortIndex";
+                        sql = string.Format(sql, userid, Request["id"]);
+                    }
+                    result = "[";
+                    if (!string.IsNullOrEmpty(sql))
+                    {
+                        DataTable dt = DBMgr.GetDataTable(sql);
+                        int i = 0;
+                        string children = string.Empty;
+                        foreach (DataRow dr in dt.Rows)
+                        {
+                            children = getchildren(dr["MODULEID"].ToString(), userid);
+                            if (i != dt.Rows.Count - 1)
+                            {
+                                result += "{id:'" + dr["MODULEID"] + "',name:'" + dr["NAME"] + "',ParentID:'" + dr["PARENTID"] + "',leaf:'" + dr["ISLEAF"] + "',checked:" + (string.IsNullOrEmpty(dr["AUTHORITY"] + "") ? "false" : "true") + ",children:" + children + "},";
+                            }
+                            else
+                            {
+                                result += "{id:'" + dr["MODULEID"] + "',name:'" + dr["NAME"] + "',ParentID:'" + dr["PARENTID"] + "',leaf:'" + dr["ISLEAF"] + "',checked:" + (string.IsNullOrEmpty(dr["AUTHORITY"] + "") ? "false" : "true") + ",children:" + children + "}";
+                            }
+                            i++;
+                        }
+                    }
+                    result += "]";
+                    Response.Write(result);
+                    Response.End();
+                    break;
+                case "loaduser":
+                    sql = "SELECT * FROM sys_user where ENABLED=1 and type!=4 order by name";
+                    ents = DBMgr.GetDataTable(sql);
+                    result = "{rows:" + JsonConvert.SerializeObject(ents) + "}";
+                    Response.Write(result);
+                    Response.End();
+                    break;
+                case "AuthorizationSave":
+                    string moduleids = Request["moduleids"];
+                    sql = @"DELETE FROM SYS_MODULEUSER WHERE USERID = '{0}'";
+                    sql = string.Format(sql, userid);
+                    DBMgr.ExecuteNonQuery(sql);
+                    string[] ids = moduleids.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (string moduleid in ids)
+                    {
+                        sql = @"insert into SYS_MODULEUSER (USERID,MODULEID) values ('{0}','{1}')";
+                        sql = string.Format(sql, userid, moduleid);
+                        DBMgr.ExecuteNonQuery(sql);
+                    }
+
+                    SysModule obj2 = new SysModule();
+                    obj2.id = "91a0657f-1939-4528-80aa-91b202a593ac";
+                    obj2 = GetTree(null, obj2, userid);
+                    string json = JsonConvert.SerializeObject(obj2.children, Formatting.None);
+                    json = json.Replace("check", "checked");
+
+                    updateChildrenAuthority();
+
+                    Response.Write("{success:true}");
+                    Response.End();
+                    break;
+            }
+        }
+
+        private void updateChildrenAuthority()
+        {
+            try
+            {
+                //主账号的ModuleId集合
+                DataTable moduleIdDt = DBMgr.GetDataTable("select MODULEID from SYS_MODULEUSER where userid = " + userid);
+                List<string> mIdList = Extension.getColumnFromDatatable(moduleIdDt, "MODULEID");
+
+                //子账号ID集合
+                DataTable cdIds = DBMgr.GetDataTable("select ID from SYS_USER where parentid = " + userid);
+
+                //遍历子账号
+                foreach (DataRow drId in cdIds.Rows)
+                {
+                    //查询该子账号的moduleIds
+                    DataTable mIdsDt = DBMgr.GetDataTable("select MODULEID from SYS_MODULEUSER where userid = " + drId["ID"].ToString());
+
+                    //删除该子账号原有的moduleIds
+                    DBMgr.ExecuteNonQuery("DELETE FROM SYS_MODULEUSER WHERE USERID = " + drId["ID"].ToString());
+
+                    //插入子账号moduleIds
+                    foreach (DataRow mId in mIdsDt.Rows)
+                    {
+                        //过滤掉子账号中主账号没有的moduleIds
+                        if (mIdList.Contains(mId["MODULEID"].ToString()))
+                        {
+                            DBMgr.ExecuteNonQuery("insert into SYS_MODULEUSER (USERID,MODULEID) values ('" + drId["ID"].ToString() + "','" + mId["MODULEID"].ToString() + "')");
+                        }
+                    }
+
+                    //创建子账号权限配置数据集缓存
+                    SysModule obj2 = new SysModule();
+                    obj2.id = "91a0657f-1939-4528-80aa-91b202a593ac";
+                    obj2 = GetTree(moduleIdDt, obj2, drId["ID"].ToString());
+                    string json = JsonConvert.SerializeObject(obj2.children, Formatting.None);
+                    json = json.Replace("check", "checked");
+                }
+            }
+            catch (Exception e)
+            {
+                //log.Error(e.Message + e.StackTrace);
+            }
+
+            Response.Write("{success:true}");
+            Response.End();
+
+        }
+
+        private SysModule GetTree(DataTable moduleIdDt, SysModule obj, string userid)
+        {
+            try
+            {
+                string strSQL = @"select a.MODULEID as id,a.NAME,a.ISLEAF,a.URL,a.PARENTID,a.SORTINDEX,
+                                      (select  b.ModuleId  from SYS_MODULEUSER b where a.MODULEID=b.MODULEID AND B.USERID='" + userid + "' and rownum=1) as CHECKED from sysmodule a  " +
+                                      "where PARENTID ='" + obj.id + "' order by SORTINDEX";
+                DataTable dtNext = DBMgr.GetDataTable(strSQL);
+                obj.children = new List<SysModule>();
+
+                foreach (DataRow dr in dtNext.Rows)
+                {
+
+                    if (moduleIdDt != null)
+                    {
+                        //过滤掉主账号没有的ModuleId
+                        int count = 0;
+                        foreach (DataRow mdr in moduleIdDt.Rows)
+                        {
+                            if (mdr["MODULEID"].ToString() == dr["id"].ToString())
+                            {
+                                count += 1;
+                            }
+                        }
+                        if (count == 0)
+                        {
+                            continue;
+                        }
+                    }
+
+                    SysModule st = new SysModule();
+                    st.id = dr["id"].ToString();
+                    st.name = dr["name"].ToString();
+                    st.ParentID = dr["PARENTID"].ToString();
+                    st.check = string.IsNullOrEmpty(dr["CHECKED"].ToString()) ? false : true;
+                    // 递归调用
+                    st = this.GetTree(moduleIdDt, st, userid);
+                    st.leaf = dr["ISLEAF"] + "";
+                    obj.children.Add(st);
+                }
+                return obj;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        private string getchildren(string moduleid, string userid)
+        {
+            string children = "[";
+            sql = @"select t.*,u.MODULEID AUTHORITY from sysmodule t left join (select * from sys_moduleuser where userid='{0}') u on t.MODULEID=u.MODULEID
+                where  t.ParentId ='{1}' order by t.SortIndex";
+            sql = string.Format(sql, userid, moduleid);
+            DataTable dt = DBMgr.GetDataTable(sql);
+            int i = 0;
+            foreach (DataRow dr in dt.Rows)
+            {
+                string tmp_children = getchildren(dr["MODULEID"].ToString(), userid);
+                if (i != dt.Rows.Count - 1)
+                {
+                    children += "{id:'" + dr["MODULEID"] + "',name:'" + dr["NAME"] + "',ParentID:'" + dr["PARENTID"] + "',leaf:'" + dr["ISLEAF"] + "',checked:" + (string.IsNullOrEmpty(dr["AUTHORITY"] + "") ? "false" : "true") + ",children:" + tmp_children + "},";
+                }
+                else
+                {
+                    children += "{id:'" + dr["MODULEID"] + "',name:'" + dr["NAME"] + "',ParentID:'" + dr["PARENTID"] + "',leaf:'" + dr["ISLEAF"] + "',checked:" + (string.IsNullOrEmpty(dr["AUTHORITY"] + "") ? "false" : "true") + ",children:" + tmp_children + "}";
+                }
+                i++;
+            }
+            children += "]";
+            return children;
+        }
+    }
+}
